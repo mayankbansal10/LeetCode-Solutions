1421a174551cdef5b6fd3cac88a31eb6baeebb3d
Mayank Bansal <mayankbansal10@gmail.com>
2023-07-08 17:49:50 -0400

    Blind 75 Python Problem

---
+++ Python/11. Container With Most Wate.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        l = 0
+        r = len(height) - 1
+        maxArea = 0
+        while l < r:
+            area = min(height[l], height[r]) * (r - l)
+            maxArea = max(area, maxArea)
+
+            if height[l] > height[r]:
+                r -= 1
+            else:
+                l += 1
+
+        return maxArea
