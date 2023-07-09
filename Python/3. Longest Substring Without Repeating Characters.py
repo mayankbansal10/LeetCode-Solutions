3d077158987de2069c2498dc20bb955b0760ff71
Mayank Bansal <mayankbansal10@gmail.com>
2023-07-09 19:48:15 -0400

    Blind 75 Python Problem

---
+++ Python/3. Longest Substring Without Repeating Characters.py
@@ -0,0 +1,17 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        ans = 0
+        l = 0
+        seen = {}
+
+        for r in range(len(s)):
+            if s[r] not in seen:
+                ans = max(ans, r-l+1)
+            else:
+                if seen[s[r]] < l:
+                    ans = max(ans, r-l+1)
+                else:
+                    l = seen[s[r]] + 1
+            seen[s[r]] = r
+
+        return ans
