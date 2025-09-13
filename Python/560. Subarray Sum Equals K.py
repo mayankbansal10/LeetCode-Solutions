8d35ee0ad4301e4625c7c968e8a991dae1c6a6c9
Mayank Bansal <mayankbansal10@gmail.com>
2025-09-13 13:57:27 -0700

    Create 560. Subarray Sum Equals K.py

---
+++ Python/560. Subarray Sum Equals K.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def subarraySum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        count, prefix_sum = 0, 0
+        prefix_sum_count = {0: 1}
+
+        for num in nums:
+            prefix_sum += num
+
+            if (prefix_sum - k) in prefix_sum_count:
+                count += prefix_sum_count[prefix_sum - k]
+            if prefix_sum in prefix_sum_count:
+                prefix_sum_count[prefix_sum] += 1
+            else:
+                prefix_sum_count[prefix_sum] = 1
+        
+        return count
+
+        
